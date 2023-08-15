296f575ff5814e7231fcae9fd1cd4cf5db7713b7
AstroM <menluyao92@hotmail.com>
2023-08-15 02:33:17 -0700

    Update test.py

--- Distributed_Web_Crawler_Design/test.py
+++ Distributed_Web_Crawler_Design/test.py
@@ -1,11 +1,12 @@
-import requests
+import re, requests
 from bs4 import BeautifulSoup
+
 
 def get_app_details(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
     }
-    
+
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -23,21 +24,37 @@ def get_app_details(url):
     # App Description
     app_description = soup.find('div', {'data-g-id': "description"}).text.strip()
 
-    # Rating Score
-    rating_score = soup.find('div', class_="jILTFe").div.text.strip()
+    # Extracting the rating score using a more robust method:
+    rating_div = soup.find('div', {'aria-label': re.compile(r"Rated")})
+    if rating_div:
+        potential_rating = rating_div.find_previous('div')
+        if potential_rating:
+            rating_score = potential_rating.get_text(strip=True)
+        else:
+            rating_score = "Not found"
+    else:
+        rating_score = "Not found"
 
-    # Similar Apps Names
-    similar_apps_section = soup.find('section', jscontroller="NkbkFd")
-    similar_apps_names = [app.span.text.strip() for app in similar_apps_section.find_all('span', class_="DdYX5")]
+    # Extracting similar apps names and IDs:
+    similar_apps_links = soup.select('a[href*="/store/apps/details?"]')
+    # Parsing the similar apps' IDs from the href attributes
+    similar_apps_ids = [link.get('href').split('=')[-1] for link in similar_apps_links]
+    similar_apps_names = [link.find('span', string=True).text for link in similar_apps_links if
+                          link.find('span', string=True)]
+
+    # Creating the dictionary of similar apps using app IDs as keys and app names as values:
+    similar_apps_dict = dict(zip(similar_apps_ids, similar_apps_names))
 
     return {
         'App Name': app_name,
         'Download Count': download_count,
         'App Description': app_description,
         'Rating Score': rating_score,
-        'Similar Apps Names': similar_apps_names
+        'Similar Apps': similar_apps_dict
     }
 
+
 url = "https://play.google.com/store/apps/details?id=com.deepl.mobiletranslator"
+url = "https://play.google.com/store/apps/details?id=com.ebay.mobile"
 app_details = get_app_details(url)
 print(app_details)
